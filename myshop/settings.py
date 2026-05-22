338249007e368909a5f447f614c9f31b7cf80502
soumiyachacko-dev <soumiyachacko@gmail.com>
2026-05-22 14:54:35 +0530

    Fix production deployment settings

--- myshop/settings.py
+++ myshop/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3@am1inbnoiq%e0z_d)s(huti#+*&79l#fgdxt#b-8$n5#d7ex'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
